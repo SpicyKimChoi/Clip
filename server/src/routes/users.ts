5e95a3eac1184abdb3cdc680cf5fc9dbe49cdac6
wonjun0120 <wonjundero@gmail.com>
2021-01-15 21:54:13 +0900

    :zap: [Add] 로그인 라우터 추가, 포스트맨 테스트 통과

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -4,3 +4,4 @@ import { usersController } from "../controllers/users";
 export const usersRouter = Router();
 
 usersRouter.post('/signup', usersController.signUp);
+usersRouter.post('/signin', usersController.signIn);
