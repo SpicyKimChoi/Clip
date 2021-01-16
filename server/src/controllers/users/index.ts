e483df97b35d21dcf891e8fa924627fc6d604d3d
wonjun0120 <wonjundero@gmail.com>
2021-01-16 15:44:04 +0900

    :art: [Add] 로그아웃 추가

--- server/src/controllers/users/index.ts
+++ server/src/controllers/users/index.ts
@@ -1,9 +1,11 @@
 import { signUp } from "./signup";
-import { signIn } from "./signin"
-import { withdrawal } from "./withdrawal"
+import { signIn } from "./signin";
+import { withdrawal } from "./withdrawal";
+import { signOut } from "./signout";
 
 export const usersController = {
 	signUp,
 	signIn,
 	withdrawal,
+	signOut
 }
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -6,3 +6,4 @@ export const usersRouter = Router();
 usersRouter.post('/signup', usersController.signUp);
 usersRouter.post('/signin', usersController.signIn);
 usersRouter.post('/withdrawal', usersController.withdrawal);
+usersRouter.post('/signout', usersController.signOut);
