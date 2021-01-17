e2ee8856aeb1b8c4390c7902384c8342742f2b13
Donghun kim <kisses217@gmail.com>
2021-01-18 08:46:53 +0900

    :heavy_plus_sign: [Add]리덕스 추
    리덕스 추가

---
+++ client/src/hooks/useLogin.tsx
@@ -0,0 +1,17 @@
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../modules/index";
+import { login, logout } from "../modules/Login";
+
+export default function useLogin() {
+  const dispatch = useDispatch();
+  const isLogin = useSelector((state: RootState) => state.checkLogin.isLogin);
+  const onLogin = useCallback(() => dispatch(login()), [dispatch]);
+  const onLogout = useCallback(() => dispatch(logout()), [dispatch]);
+
+  return {
+    isLogin,
+    onLogin,
+    onLogout,
+  };
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/pages/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider >,
   document.getElementById('root')
 );
 
