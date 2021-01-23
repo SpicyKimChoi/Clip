5a5867bba1401632797794b701c825a8a92d8470
wonjun0120 <wonjundero@gmail.com>
2021-01-23 21:48:24 +0900

    :zap: [Fix] 권한 leftjoin추가

--- server/src/repositories/ProjectsRepo.ts
+++ server/src/repositories/ProjectsRepo.ts
@@ -5,34 +5,28 @@ import { ProjectPermissions } from "../entity/ProjectPermissons";
 
 @EntityRepository(Projects)
 export class ProjectsRepository extends AbstractRepository<Projects> {
-	
+
 	//프로젝트 생성
-	createProject(name: string):Promise<Projects> {
+	createProject(name: string): Promise<Projects> {
 		try {
 			const project = new Projects();
 			project.name = name;
 			return this.manager.save(project);
-			
+
 		} catch (err) {
 			console.log(err);
 			return err;
 		}
 	}
-	
+
 	async getProjects(userUuid: string): Promise<Projects[]> {
 		try {
 			const userRepository = getRepository(Users);
-			const user = await userRepository.findOne({uuid: userUuid});
+			const user = await userRepository.findOne({ uuid: userUuid });
 
-			return this.repository.createQueryBuilder("proj")
-				.where(qb => {
-					qb.subQuery()
-						.select("pp.project_id")
-						.from(ProjectPermissions, "pp")
-						.where("pp.user_id = :user_id")
-						.getQuery()
-				})
-				.setParameter("user_id", user.id)
+			return this.repository.createQueryBuilder("p")
+				.leftJoinAndSelect("p.projectPermissions", "pp")
+				.where("pp.user_id = :user_id", {user_id: user.id})
 				.getMany()
 
 		} catch (err) {
