bfa35e2aec4b67be2ecaa69424609008eb666ac4
김현규 <46562138+codeFabian@users.noreply.github.com>
2021-01-23 23:21:05 +0900

    :rocket: [Add] 전체 레이아웃 변경완료 (#41)

--- client/src/components/pages/App.tsx
+++ client/src/components/pages/App.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
-import PrivateClip from "../templates/PrivateClip"
 import GlobalStlyes from "../GlobalStyle"
-import PublicClip from '../templates/PublicClip';
-import Inbox from '../templates/Inbox';
-import KanbanBoard from '../templates/KanbanBoard';
-
+import Header from "../templates/top/Header"
+import Combine from "../templates/Combine";
 
 function App() {
   return (
     <Page>
       <GlobalStlyes />
-      <PrivateClip />
-      <PublicClip />
-      <Inbox />
-      <KanbanBoard />
+      <Header />
+      <Combine />
     </Page>
   );
 }
-
 
 const Page = styled.div`
   display: grid;
   border: 5px solid;
   border-color: black;
-  grid-template-columns: 4fr 6fr;
-  grid-template-rows: 30vh 70vh;
+  grid-template-rows: 10vh 90vh;
   margin: 0px;
   grid-gap: 10px;
 `
-
 export default App;
 
