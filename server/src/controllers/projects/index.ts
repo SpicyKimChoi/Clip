09f193d538ff16eaabc0e1d583ad616eb08fbb32
wonjun0120 <wonjundero@gmail.com>
2021-01-24 13:58:20 +0900

    :art: [Add] 프로젝트 수정 API 작성

--- server/src/controllers/projects/index.ts
+++ server/src/controllers/projects/index.ts
@@ -1,8 +1,9 @@
 import { create } from "./create";
 import { getProjects } from "./getProjects";
+import { update } from "./update";
 
 export const projectsController = {
 	create,
 	getProjects,
-
+	update,
 }
