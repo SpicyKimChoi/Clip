571aebef32153f83a5daa905b962467986ac9f9e
Donghun kim <kisses217@gmail.com>
2021-01-25 10:53:19 +0900

    :heavy_plus_sign: [Add] 구글로그인 버튼 헤더에 추가
    구글로그인 버튼 및 로그아웃 버튼 헤더에 추가

--- client/src/components/atoms/LoginGoogle.tsx
+++ client/src/components/atoms/LoginGoogle.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { GoogleLogin } from "react-google-login";
 import axios from "axios";
 import * as dotenv from "dotenv";
+import useLogin from "../../hooks/useLogin";
 dotenv.config();
 
 const LoginGoogle = () => {
+  const { onLogin } = useLogin();
   const responseGoogle = (res: any): void => {
     console.log(res);
+    onLogin();
   };
   const responseFailGoogle = (err: any): void => {
     console.log(err);
--- client/src/components/templates/top/Header.tsx
+++ client/src/components/templates/top/Header.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 import styled from "styled-components";
-
+import LoginGoogle from "../../atoms/LoginGoogle";
+import LogoutButton from "../../atoms/LogoutButton";
 
 const Header = () => {
-    return (
-        <div>
-            헤더구역
-        </div>
-    );
+  return (
+    <div>
+      <LoginGoogle />
+      <LogoutButton />
+    </div>
+  );
 };
-
 
 export default Header;
