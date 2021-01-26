5171e116df1b9685d5867f1018a114afe7a587eb
codeFabian <yolo.fabian.k@gmail.com>
2021-01-27 00:25:09 +0900

    :rocket: [Add] 인피니티 스크롤 50 %

--- client/src/components/templates/top/Header.tsx
+++ client/src/components/templates/top/Header.tsx
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import InfiniteScroll from "react-infinite-scroll-component";
 import LoginGoogle from "../../atoms/LoginGoogle";
 import axios from 'axios'
+import { setgid } from 'process';
 
 const Header = () => {
-    const [data, setData] = useState([]);
+    const [addData, setAddData] = useState(8);
+    const [prevData, setPrevData] = useState(0);
+    const [data, setData] = useState(addData); // 4 or 8
+    const [hasMore, setHasMore] = useState(true);
+    const [items, setItems] = useState([] as any);
+
     const fetchMoreData = () => {
         setTimeout(() => {
-            axios.get(`https://jsonplaceholder.typicode.com/posts`)
+            axios.get(`https://koreanjson.com/todos`)
                 .then((res) => {
                     // console.log("res.data:", res.data);
+                    let fourData = res.data.slice(prevData, data);
                     //updating data
-                    setData(res.data.slice(0, 5))
+                    setItems([...items, ...fourData]);
+                    setPrevData(prevData + addData);
+                    setData(data + addData);
                 })
         }, 1000); // 시간 차 주지 않으면 스크롤 계속 내려감!
     }
     return (
         <div>
-            <div>demo: 인피니티스크롤</div>
+            <p>여기서 시작</p>
             <InfiniteScroll
-                dataLength={data.length}
+                dataLength={items.length} //This is important field to render the next data
                 next={fetchMoreData}
-                hasMore={true}
-                loader={<div>Loading...</div>}
+                hasMore={hasMore}
+                loader={<h4>Loading...</h4>}
+                scrollableTarget="scrollableDiv"
             >
-                {data.map((i, idx) => {
-                    <div key={idx}>div-#{idx}</div>
+                {items && items.map((data: any, i: any) => {
+                    { console.log(data) }
                 })}
             </InfiniteScroll>
-            <LoginGoogle />
         </div>
     );
-
-};
+}
 
 export default Header;
