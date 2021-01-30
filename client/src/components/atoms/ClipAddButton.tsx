7170b14c396b03836dbe8443768c8ce1fca8af0e
Donghun kim <59818904+rlaehdgns217@users.noreply.github.com>
2021-01-31 02:35:34 +0900

    :memo: [Add] 클립 생성 (#80)
    클립 생성

--- client/src/components/atoms/ClipAddButton.tsx
+++ client/src/components/atoms/ClipAddButton.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useModal from "../../hooks/useModal";
 import Modal from "react-modal";
+import styled from "styled-components";
 Modal.setAppElement("#root");
-
+type Clip = {
+  title: string;
+  url: string;
+  discription: string;
+};
 const customStyles = {
   content: {
     top: "50%",
@@ -13,28 +18,97 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
-
 const ClipAddButton = () => {
+  const data: Clip[] = [];
   const { isOpen, onOpen, onClose } = useModal();
+  const [inputs, setInputs] = useState({
+    title: "",
+    url: "",
+    discription: "",
+  });
+  const [clipData, setClipData] = useState<Clip[]>([]);
+  const { title, url, discription } = inputs;
+  const onClick = () => {
+    console.log("클립 생성!", inputs);
+    let temp = clipData.slice();
+    temp.push(inputs);
+    setClipData(temp);
+    console.log(data, "data check");
+    onClose();
+    onReset();
+    return data;
+  };
+  const onChange = (e: any) => {
+    const { name, value } = e.target;
+    const nextInputs = {
+      ...inputs,
+      [name]: value,
+    };
+    console.log(nextInputs);
+    setInputs(nextInputs);
+  };
+  const onReset = () => {
+    const resetInputs = {
+      title: "",
+      url: "",
+      discription: "",
+    };
+    setInputs(resetInputs);
+  };
   return (
     <>
       <button onClick={onOpen}>+</button>
+      {clipData.length === 0 ? (
+        <div></div>
+      ) : (
+        clipData.map((clip: Clip, idx: number) => {
+          console.log(clip, "clip");
+          return (
+            <Clip key={idx}>
+              <div>{clip.title}</div>
+              <a href={clip.url} target="_blank">
+                링크
+              </a>
+              <div>{clip.discription}</div>
+              <button>삭제</button>
+              <button>수정</button>
+            </Clip>
+          );
+        })
+      )}
       <Modal isOpen={isOpen} onRequestClose={onClose} style={customStyles}>
         <div>
-          <input type="text" placeholder="Title"></input>
+          <input
+            name="title"
+            placeholder="Title"
+            onChange={onChange}
+            value={title}
+          ></input>
         </div>
         <div>
-          <input type="text" placeholder="Url"></input>
+          <input
+            name="url"
+            placeholder="Url"
+            onChange={onChange}
+            value={url}
+          ></input>
         </div>
         <div>
-          <input type="text" placeholder="Discription"></input>
+          <input
+            placeholder="Discription"
+            name="discription"
+            onChange={onChange}
+            value={discription}
+          ></input>
         </div>
-
         <button onClick={onClose}>취소</button>
-        <button onClick={() => console.log("클립 생성!")}>생성</button>
+        <button onClick={onClick}>생성</button>
       </Modal>
     </>
   );
 };
-
+const Clip = styled.div`
+  border: 1px solid;
+  border-color: green;
+`;
 export default ClipAddButton;
