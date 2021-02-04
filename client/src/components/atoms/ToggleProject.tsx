0db7b48f429303bc1f51bd0c9faccfd39903a263
codeFabian <yolo.fabian.k@gmail.com>
2021-02-04 15:39:42 +0900

    :rocket: [Add] 프로젝트 토글 목업 완료

---
+++ client/src/components/atoms/ToggleProject.tsx
@@ -0,0 +1,38 @@
+import React, { useState, SetStateAction } from 'react';
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Button
+} from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons'
+
+
+const ToggleProject = () => {
+    const [select, setSelect] = useState("Your Project")
+    const toggle = React.useRef<HTMLButtonElement>(null)
+    const handleProject = (e: any) => {
+        setSelect(e.target.value)
+    }
+
+    console.log(select)
+    return (
+        <>
+            <Menu>
+                <MenuButton ref={toggle} as={Button} mr={3} rightIcon={<ChevronDownIcon />}>
+                    {select}
+                </MenuButton>
+                <MenuList onClick={handleProject} >
+                    <MenuItem value="Download">Download</MenuItem>
+                    <MenuItem value="Create a Copy">Create a Copy</MenuItem>
+                    <MenuItem value="Mark as Draft">Mark as Draft</MenuItem>
+                    <MenuItem value="Delete">Delete</MenuItem>
+                    <MenuItem value="Attend a Workshop">Attend a Workshop</MenuItem>
+                </MenuList>
+            </Menu>
+        </>
+    );
+};
+
+export default ToggleProject;
--- client/src/components/templates/top/Header.tsx
+++ client/src/components/templates/top/Header.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import LoginGoogle from "../../atoms/LoginGoogle";
 import CreateProject from "../../atoms/CreateProject"
-
+import ToggleProject from "../../atoms/ToggleProject";
 
 
 const Header = () => {
     return (
         <div>
             <LoginGoogle />
+            <ToggleProject />
             <CreateProject />
         </div>
     )
