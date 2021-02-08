e86fbb0972195af07866b343152be08ebc2af2f8
Donghun kim <59818904+rlaehdgns217@users.noreply.github.com>
2021-02-08 21:28:47 +0900

    :memo: [Add] D&D 추가 (#89)
    private clip d&d 추가

--- client/src/components/templates/contents/PrivateClip.tsx
+++ client/src/components/templates/contents/PrivateClip.tsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import Clip from "../../../components/atoms/Clip";
 import ClipAddButton from "../../../components/atoms/ClipAddButton";
 import usePrivateClip from "../../../hooks/usePrivateClip";
 import { Input } from "../../../modules/PrivateClip";
+import { useDrop } from "react-dnd";
+import update from "immutability-helper";
 
 const PrivateClip = () => {
-  const { privateClipArr } = usePrivateClip();
+  const { privateClipArr, changePrivateClip } = usePrivateClip();
+  const findCard = (id: string) => {
+    const clip = privateClipArr.filter((c) => `${c.id}` === id)[0];
+    return {
+      clip,
+      index: privateClipArr.indexOf(clip),
+    };
+  };
+  const moveClip = (id: string, atIndex: number) => {
+    const { clip, index } = findCard(id);
+    changePrivateClip(
+      update(privateClipArr, {
+        $splice: [
+          [index, 1],
+          [atIndex, 0, clip],
+        ],
+      }),
+    );
+  };
+  const ItemTypes = { CLIP: "clip" };
+  const [, drop] = useDrop({ accept: ItemTypes.CLIP });
+
   return (
-    <PrivateClipGrid>
+
+    <PrivateClipGrid ref={drop}>
+
       <ClipAddButton />
       {privateClipArr.map((clip: Input, idx: number) => {
         return (
           <Clip
             key={idx}
             title={clip.title}
-            id={clip.id}
+            id={`${clip.id}`}
             url={clip.url}
             discription={clip.discription}
+            moveClip={moveClip}
+            findClip={findCard}
           />
         );
       })}
